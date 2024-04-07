a54c8c868e6d81c080fc12f3d6cf03115c84ac03
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-07 21:03:01 +0530

    Add Local storage based API and Handle GET requests

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+const fs = require("fs");
 const express = require("express");
-const app = express();
+const app = express(); // Created to get the bunch of values we get to work with upon calling express.
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello! I am the Server...", API_Name: "Natours" });
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello! I am the Server...", API_Name: "Natours" });
+// });
+
+// Reading Data
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+console.log(tours);
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours
+      // endpoint : variable
+    }
+  })
 });
 
 app.post("/", (req, res) => {
-  res.send("You cannot post to this endpoint...");
+  res.send("You can post to this endpoint...");
 });
 
 app.listen(port, () => {
