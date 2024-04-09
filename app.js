3d43662c0e04a497b016acbf4c1f24f7aeb47cc5
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-09 22:28:21 +0530

    Handle Post request

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const express = require("express");
 const app = express(); // Created to get the bunch of values we get to work with upon calling express.
 const port = 3000;
-
+// Adding Middleware
+app.use(express.json());
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -13,20 +14,33 @@ const port = 3000;
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-console.log(tours);
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
     data: {
-      tours: tours
+      tours: tours,
       // endpoint : variable
-    }
-  })
+    },
+  });
 });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+app.post("/api/v1/tours", (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
