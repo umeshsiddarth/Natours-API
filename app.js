dae6909b8d2c9dcb2eccf99e01e7aa1b9f99a952
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-10 21:23:59 +0530

    Respond to URL parameters

--- app.js
+++ app.js
@@ -25,6 +25,34 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// Responding to URL parameters
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = Number(req.params.id);
+  // Condition to check whether the requested id is present on not using the id itself and respond back as failed request
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   });
+  // }
+  const tour = tours.find((el) => el.id === id);
+
+  // Consition to check whether there is a tour or not and respond back as failed request
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours: tour,
+    },
+  });
+});
+
 app.post("/api/v1/tours", (req, res) => {
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
