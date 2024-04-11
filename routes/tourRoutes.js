4c899aa2ce18ccfe62036fa00ad497ce88034c56
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-11 21:19:21 +0530

    Refactor the code and add separate files and folders

---
+++ routes/tourRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const tourControllers = require("../controllers/tourController");
+// Routes
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello! I am the Server...", API_Name: "Natours" });
+// });
+
+// app.get("/api/v1/tours", getAllTours);
+// Responding to URL parameters
+// app.get("/api/v1/tours/:id", getTour);
+// app.post("/api/v1/tours", createTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+// Refactoring the above methods as below using chaining methods
+
+// app.route("/api/v1/tours").get(getAllTours).post(createTour);
+// app
+//   .route("/api/v1/tours/:id")
+//   .get(getTour)
+//   .patch(updateTour)
+//   .delete(deleteTour);
+
+// app.route("/api/v1/users").get(getAllUsers).post(createUser);
+// app
+//   .route("/api/v1/users/:id")
+//   .get(getUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
+
+// Cleaning the code based on Mounting Router method and using middlewares
+const router = express.Router();
+
+router
+  .route("/")
+  .get(tourControllers.getAllTours)
+  .post(tourControllers.createTour);
+router
+  .route("/:id")
+  .get(tourControllers.getTour)
+  .patch(tourControllers.updateTour)
+  .delete(tourControllers.deleteTour);
+
+module.exports = router;
