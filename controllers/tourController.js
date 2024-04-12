21030685813cfdce3b1052381ee2a8f427766327
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-12 20:11:01 +0530

    Add chaining middleware and serving static files
    Chained checkBody middleware to post tour method and
    added express' static middleware to serve static files from
    public folder.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,15 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Properties",
+    });
+  }
+  next();
+};
 // Route Handlers
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
