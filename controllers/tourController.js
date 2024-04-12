99860eccbf9b89cf21ccc27edfa78579fa2a2c02
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-04-12 11:07:39 +0530

    Refactor code by implementing Params Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (Number(req.params.id) > tours.length - 1) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  next();
+};
+
 // Route Handlers
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -22,6 +32,8 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = Number(req.params.id);
   // Condition to check whether the requested id is present on not using the id itself and respond back as failed request
+  // We use this condition in middleware thus commented here and for consistence the next if method is avoided. That just acts
+  //  as a reference to possibilities
   // if (id > tours.length-1) {
   //   return res.status(404).json({
   //     status: "fail",
@@ -30,13 +42,13 @@ exports.getTour = (req, res) => {
   // }
   const tour = tours.find((el) => el.id === id);
 
-  // Consition to check whether there is a tour or not and respond back as failed request
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
+  // Condition to check whether there is a tour or not and respond back as failed request
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "Invalid ID",
+  //   });
+  // }
 
   res.status(200).json({
     status: "success",
@@ -65,12 +77,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length - 1) {
-    res.status(404).json({
-      status: "fail",
-      message: "Inavli ID",
-    });
-  }
   res.status(200).json({
     status: "success",
     data: {
@@ -80,13 +86,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length - 1) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     data: null,
