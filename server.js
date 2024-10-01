c29577175bba7023bad36b49fd60c716564b8caf
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-10-01 18:01:30 +0530

    Add mongo DB Basics

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then((con) => console.log("Connected"));
+
+// Creating a Schema
+const tourSchema = new mongoose.Schema({
+  // name: String, // Basic way of defining
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"], // true and error, this is called validator
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+// Creating a Model
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "Dharamshala",
+  price: 100,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
