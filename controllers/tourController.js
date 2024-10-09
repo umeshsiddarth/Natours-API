99552b9fbd7c6897195fd9d738af62f8f9bb4a72
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-10-09 19:06:52 +0530

    Implement filtering and advanced filtering with regex

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,41 @@ const Tour = require("../model/tourModel");
 // Route Handlers
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY
+
+    // 1) Filtering
+    //Creating a shallow copy of the query object to manipulate using destructuring
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "fields", "limit"];
+
+    excludeFields.forEach((el) => {
+      delete queryObj[el];
+    });
+
+    // 2) Advanced Filtering
+    // The URL: domain.com/api/v1/tours?duration[gte]=5&difficulty=easy
+    // This is how a normal query looks like in mongo
+    // {difficulty: 'easy', duration: { $gte: 5 }}
+    // But when we get the query from req.query we get
+    // {difficulty: 'easy', duration: { gte: 5 }}
+    //  The $ sign is missing infront of gte
+
+    // So we use simple JS with regex to replace the query with the $ sign in front
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => {
+      return `$${match}`;
+    });
+    // console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
     // find() is used for querying all data from a doc
-    const tours = await Tour.find();
+    // const tours = await Tour.find();
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
