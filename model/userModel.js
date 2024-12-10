d31d5ce3e779b78ed7e1034c3e303aad86b6c3a6
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-12-10 11:32:27 +0530

    Add User model

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,12 +33,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please confirm your password"],
     validate: {
-      // Imp This only works with SAVE!!!
+      // Imp This only works on CREATE or SAVE!!!
       validator: function (el) {
         return el === this.password;
       },
+      message: "Passwords doesn't match",
     },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  // hash is async function
+  this.password = await bcrypt.hash(this.password, 12);
+  // We use salt length/cost value (2nd param), the random number to be added and hashed. The default cost value 10 but because of powerful computers available these days we use 12
+  this.confirmPassword = undefined;
+  // We don't want confirm password anymore in database, it is there just to help users.
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
